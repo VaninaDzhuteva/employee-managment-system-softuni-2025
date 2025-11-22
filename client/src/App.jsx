9fa2cdbece4bd1703154306e59f139abfa311240
vdzhuteva <v.k.dzhuteva@gmail.com>
2025-11-22 15:57:23 +0200

    Add base components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Search from './components/Search.jsx'
+import UserList from './components/UserList.jsx'
+import Pagination from './components/Pagination.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Header />
+      <main className="main">
+        <section className="card users-container">
+        <Search />
+
+        <UserList />
+
+        <Pagination />
+
+        </section>
+      </main>
+
+      <Footer />
+    </div>
   )
 }
 
