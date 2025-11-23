8c8650434a7634c25b27f9cca9311ec846cebf03
vdzhuteva <v.k.dzhuteva@gmail.com>
2025-11-23 13:32:16 +0200

    Convert date

--- client/src/components/UserItem.jsx
+++ client/src/components/UserItem.jsx
@@ -1,3 +1,5 @@
+import { fromIsoDate } from "../utils/datetimeUtils.js";
+
 export default function UserItem({
     _id,
     firstName,
@@ -21,7 +23,7 @@ export default function UserItem({
             <td>{lastName}</td>
             <td>{email}</td>
             <td>{phoneNumber}</td>
-            <td>{createdAt}</td>
+            <td>{fromIsoDate(createdAt)}</td>
             <td className="actions">
                 <button className="btn edit-btn" title="Edit">
                     <svg
